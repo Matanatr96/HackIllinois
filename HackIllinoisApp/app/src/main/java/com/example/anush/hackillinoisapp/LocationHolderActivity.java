8ad9646d2a088ed10943c90789bb7f8c12d37391
Anush Mattapalli <anush0620@yahoo.com>
2016-02-21 03:40:06 -0500

    final commit, scope of project is cut down by a lot, functionality is still cool nonetheless

--- HackIllinoisApp/app/src/main/java/com/example/anush/hackillinoisapp/LocationHolderActivity.java
+++ HackIllinoisApp/app/src/main/java/com/example/anush/hackillinoisapp/LocationHolderActivity.java
@@ -1,10 +1,12 @@
 package com.example.anush.hackillinoisapp;
 
+import android.content.Intent;
 import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -52,8 +54,13 @@ public class LocationHolderActivity extends AppCompatActivity {
 
             lv.setAdapter(arrayAdapter);
         }
-
     }
+
+    public void descriptiveView(View v) {
+        startActivity(new Intent(this, DescriptiveViewActivity.class));
+    }
+
+
 
 
 }
